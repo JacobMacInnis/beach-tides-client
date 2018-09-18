51eabbcbf10a6dc12434c1684059975c4138e40b
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-09-17 21:45:29 -0400

    moved environment variables from config.js to .env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 
-export const GOOGLE_CLIENT_ID = "243766734327-66q96li36m0b4re915i9vjv8jiph9u8n.apps.googleusercontent.com";
-export const FACEBOOK_APP_ID = '2076173755748034'
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "243766734327-66q96li36m0b4re915i9vjv8jiph9u8n.apps.googleusercontent.com";
