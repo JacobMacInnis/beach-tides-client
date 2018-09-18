223051d4a02097fbd76f445d3cf255ed83611980
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-09-18 10:34:55 -0400

    added beachtides-server to config

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ export const API_BASE_URL =
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "243766734327-66q96li36m0b4re915i9vjv8jiph9u8n.apps.googleusercontent.com";
 
 export const API_V1_AUTH_GOOGLE = process.env.API_V1_AUTH_GOOGLE ||
-'https://secret-lowlands-23691.herokuapp.com/api/v1/auth/google';
+'https://beachtides-server.herokuapp.com/api/v1/auth/google';
 // 'http://localhost:8080/api/v1/auth/google';
