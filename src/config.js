7bdaac6d21eda84d6a1c8843305edb084df60609
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-09-25 06:46:16 -0400

    changed config.js process.env to process.env.REACT_APP

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const API_BASE_URL =
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "243766734327-66q96li36m0b4re915i9vjv8jiph9u8n.apps.googleusercontent.com";
 
-export const API_V1_AUTH_GOOGLE = process.env.API_V1_AUTH_GOOGLE ||
+export const API_V1_AUTH_GOOGLE = process.env.REACT_APP_API_V1_AUTH_GOOGLE ||
 'http://localhost:8080/api/v1/auth/google';
 
 // 'https://beachtides-server.herokuapp.com/api/v1/auth/google';
