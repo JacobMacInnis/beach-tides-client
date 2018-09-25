8b2a37fadd8b3908e023d820b4de261b1195a2de
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-09-25 06:58:59 -0400

    added REACT_APP to all environment variables in config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-export const API_BASE_URL =
+export const REACT_APP_API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "243766734327-66q96li36m0b4re915i9vjv8jiph9u8n.apps.googleusercontent.com";
+export const REACT_APP_GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || "243766734327-66q96li36m0b4re915i9vjv8jiph9u8n.apps.googleusercontent.com";
 
-export const API_V1_AUTH_GOOGLE = process.env.REACT_APP_API_V1_AUTH_GOOGLE ||
+export const REACT_APP_API_V1_AUTH_GOOGLE = process.env.REACT_APP_API_V1_AUTH_GOOGLE ||
 'http://localhost:8080/api/v1/auth/google';
 
 // 'https://beachtides-server.herokuapp.com/api/v1/auth/google';
