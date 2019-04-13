9bc4270f2da99aabdb1998ad6d557f5fefdce64d
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-04-12 23:00:26 -0400

    clean code

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { Provider } from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
-<Provider store={store}>
-  <Router>
-    <App />
-  </Router>
-</Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
